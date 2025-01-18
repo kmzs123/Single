087b5a97cbbd6dd27ace2ed09f27029be5a5367a
空明竹随 <kmzs1023@qq.com>
2025-01-18 23:00:50 +0800

    更新fortawesome的css文件
    移除了旧版font-awesome@4.7.0的css
    添加了fortawesome@6.7.2和旧版兼容css

--- header.php
+++ header.php
@@ -16,7 +16,8 @@
 <?php endif; ?>
     <link href="<?php $this -> options -> themeUrl('static/kico.css'); ?>" rel="stylesheet" type="text/css"/>
     <link href="<?php $this -> options -> themeUrl('static/single.css'); ?>" rel="stylesheet" type="text/css"/>
-    <link href="https://fastly.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fastly.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fastly.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/v4-shims.min.css" rel="stylesheet" type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <?php if($this -> options -> background): ?>
     <style>body:before{content: ''; background-image: url(<?php $this -> options -> background(); ?>)}</style>
